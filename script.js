7a2908c862bf4aa197d462f7a260e6fffe9445da
flarxx <flarx3@gmail.com>
2025-04-07 12:20:59 -0400

    un poco de estilo

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const input = document.getElementById("text-input");
 const button = document.getElementById("check-btn");
 const input = document.getElementById("result");
 
+button.addEventListener("click"){
+    alert("please input value")
+}
